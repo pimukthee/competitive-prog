8affb995666f7547c6a8d2cf335aa52c6cf74ad3
pimukthee <pimukthee@gmail.com>
2017-06-20 10:31:32 +0700

    fixed mcs

--- algorithm/mcs.cpp
+++ algorithm/mcs.cpp
@@ -1,44 +1,23 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
-#include <utility>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <functional>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-typedef pair<int,int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
+int main() {
+  int n;
+  cin >> n;
+  vector <int> num(n + 1);
 
-template <typename T>
-using pql = priority_queue<T, vector<T>, greater<T> >; 
-template <typename T>
-using pqm = priority_queue<T>;
+  for (int i = 1; i <= n; i++)
+    cin >> num[i];
 
-#define INF 1e9
-#define mp make_pair
-#define push_back pb
+  // dp best between ch
+  int sum = 0, ans = 0;
+  for (int i = 1; i <= n; i++) {
+    sum = max(num[i], sum + num[i]);
+    ans = max(ans, sum);
+  }
 
-int main(){
-	
-	int n;
-	cin >> n;
-	int dp[n+1],num[n+1];
+  cout << ans << endl;
 
-	memset(dp,0,sizeof(dp));
-
-	for(int i=1;i<=n;i++)
-		cin >> num[i];
-
-	// dp best between ch
-	for(int i=1;i<=n;i++){
-		dp[i] = max(num[i],dp[i-1]+num[i]);	
-	}
-
-	cout << dp[n] <<endl;
-	
-	return 0;
-
+  return 0;
 }
