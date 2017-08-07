441012193e36ce6111c4acb801499e6b9d203827
pimukthee <pimukthee@gmail.com>
2017-08-07 12:42:32 +0700

    tech jam

--- techjam/3.py
+++ techjam/3.py
@@ -1,24 +1,31 @@
-possible_way = [[-1] * 305] * 15
-
-def main():
-    times = map(int, raw_input().split())
+def solution(s):
+    times = s
     number_of_customer = len(times)
-    total_time = sum(times)
+    total_time = 5
     is_subset_sum = find_all_subset_sum(times, number_of_customer, total_time)
     waiting_time = minimum_waiting_time(is_subset_sum, number_of_customer, total_time // 2, total_time)
-    print waiting_time
+    return waiting_time  
 
-def find_all_subset_sum(times, customer, total_time):
-    """dynamic programming subset sum"""
-    if possible_way[customer][total_time] >= 0:
-        return possible_way[customer][total_time]
-    if total_time == 0:
-        return 1
-    if customer == 0:
-        return 0
-    if total_time >= times[customer - 1]
-
-
+def find_all_subset_sum(times, number_of_customer, total_time):
+    #dynamic programming subset sum
+    is_subset_sum = []
+    for i in range(0, number_of_customer + 1):
+        is_subset_sum.append([False] * (total_time + 1))
+    for i in range(0, number_of_customer + 1):
+        is_subset_sum[i][0] = True
+    for i in range(1, number_of_customer + 1):
+        for j in range(1, total_time + 1):
+            is_subset_sum[i][j] = is_subset_sum[i - 1][j]
+            print i
+            print is_subset_sum
+            print
+            if j >= times[i-1]:
+                print "edit ", i, j
+                is_subset_sum[i][j] = is_subset_sum[i][j] | is_subset_sum[i-1][j-times[i-1]]
+            print is_subset_sum
+            print
+    return is_subset_sum
+ 
 def minimum_waiting_time(is_subset_sum, number_of_customer, average_time, total_time):
     waiting_time = 1000
     for time in range(average_time, total_time + 1):
@@ -27,6 +34,9 @@ def minimum_waiting_time(is_subset_sum, number_of_customer, average_time, total_
             second_queue = total_time - first_queue
             waiting_time = min(waiting_time, max(first_queue, second_queue))
     return waiting_time
+
+def main():
+    print solution([1])
 
 if __name__ == '__main__':
     main()
--- test.cpp
+++ test.cpp
@@ -1,20 +1,57 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-const int MAXN = 1e6 + 10;
-const int MOD = 1e9 + 7;
-
-int n, dp[MAXN], ans;
-bool mark[MAXN];
-
-int main() {
-    int n = 5, ans = n;
-    for (int i = 2; i * i <= n; ++i) {
-        while (n % i == 0) n /= i;
-        ans -= ans / i;
-    }
-    if (n > 1) ans -= ans / n;
-    cout << ans;
-    return 0;
+  
+// Returns true if there is a subset of set[] with sun equal to given sum
+bool isSubsetSum(int set[], int n, int sum)
+{
+    // The value of subset[i][j] will be true if there is a 
+    // subset of set[0..j-1] with sum equal to i
+    bool subset[n+1][sum+1];
+  
+    // If sum is 0, then answer is true
+    for (int i = 0; i <= n; i++)
+      subset[i][0] = true;
+  
+    // If sum is not 0 and set is empty, then answer is false
+    for (int i = 1; i <= sum; i++)
+      subset[0][i] = false;
+  
+     // Fill the subset table in botton up manner
+     for (int i = 1; i <= n; i++)
+     {
+       for (int j = 1; j <= sum; j++)
+       {
+         if(j<set[i-1])
+         subset[i][j] = subset[i-1][j];
+         if (j >= set[i-1])
+           subset[i][j] = subset[i-1][j] || 
+                                 subset[i - 1][j-set[i-1]];
+       }
+       for (int j = 1; j <= sum; ++j) cout << subset[i][j] << " ";
+       cout << endl;
+     }
+  
+     /*for (int i = 0; i <= n; i++)
+     {
+       for (int j = 0; j <= sum; j++)
+          printf ("%4d", subset[i][j]);
+       printf("\n");
+     }*/
+  
+     return subset[n][sum];
 }
+  
+// Driver program to test above function
+int main()
+{
+  int set[] = {1, 30};
+  int sum = 31;
+  int n = 2;
+  if (isSubsetSum(set, n, sum) == true)
+     printf("Found a subset with given sum");
+  else
+     printf("No subset with given sum");
+  return 0;
+}
+// This code is contributed by Arjun Tyagi.
